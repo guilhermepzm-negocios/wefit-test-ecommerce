e4ad9002f713c877876e3f29865e4d8556c7ad6a
Guilherme Martins <guilhermepzm@outlook.com>
2024-02-17 18:20:33 -0300

    fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,7 +18,7 @@ const Home: React.FC = () => {
   );
 
   useEffect(() => {
-    dispatch(listMoviesRequest());
+    if (movies.length === 0) dispatch(listMoviesRequest());
   }, []);
 
   return (
